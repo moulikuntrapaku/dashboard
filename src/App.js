afac7aa9a2a621f2d31e6589fddd2805d90c00ed
moulikuntrapaku <moulikuntrapaku@gmail.com>
2024-01-30 22:37:22 +0530

    Add Revenue component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import OrangeFarm from './OrangeFarm';
+import Revenue from './Revenue';
 
 function App() {
   return (
     <div className="App">
       <OrangeFarm/>
+      <Revenue/>
     </div>
   );
 }
---
+++ src/Revenue.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import upIcon from './images/up-icon.png'
+import rightArrow from './images/right-arrow.png'
+
+export default function () {
+  return (
+    <div>
+        <section>
+            <header>Revenues</header>
+            <div>
+                <section>
+                    <span>15%</span>
+                    <img src={upIcon} alt='up-icon'/>
+                </section>
+                <span>Increase compared to last week</span>
+            </div>
+        </section>
+        <section>
+            <span>Revenues report</span>
+            <img src={rightArrow} alt='right-arrow'/>
+        </section>
+    </div>
+  )
+}
